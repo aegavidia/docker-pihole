316355061b68abe0970bdb54d8678761808d4efe
aegavidia <36170491+aegavidia@users.noreply.github.com>
2018-02-05 14:33:03 -0500

    Update pihole.sh

--- pihole.sh
+++ pihole.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-IP_LOOKUP="$(ip route get 9.9.9.9 | awk '{ print $NF; exit }')"  # May not work for VPN / tun0
+IP_LOOKUP="$(ip route get 8.8.8.8 | awk '{ print $NF; exit }')"  # May not work for VPN / tun0
 IPv6_LOOKUP="$(ip -6 route get 2001:4860:4860::8888 | awk '{ print $10; exit }')"  # May not work for VPN / tu$
 IP="${IP:-$IP_LOOKUP}"  # use $IP, if set, otherwise IP_LOOKUP
 IPv6="${IPv6:-$IPv6_LOOKUP}"  # use $IPv6, if set, otherwise IP_LOOKUP
@@ -14,9 +14,8 @@ docker run -d \
     --network devnet \
     -v "${DOCKER_CONFIGS}/pihole/pihole/:/etc/pihole/" \
     -v "${DOCKER_CONFIGS}/pihole/dnsmasq.d/:/etc/dnsmasq.d/" \
-    -e ServerIP="${IP:-$(ip route get 9.9.9.9 | awk '{ print $NF; exit }')}" \
+    -e ServerIP="${IP:-$(ip route get 8.8.8.8 | awk '{ print $NF; exit }')}" \
     -e ServerIPv6="${IPv6:-$(ip -6 route get 2001:4860:4860::8888 | awk '{ print $10; exit }')}" \
     -e TZ="america/new_york" \
-    -e DNS1="9.9.9.9" \
     --restart=always \
     diginc/pi-hole-multiarch:debian_armhf

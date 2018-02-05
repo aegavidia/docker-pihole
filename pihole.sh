9c82da08754d39418311b943977d7c4a75cbc3b3
aegavidia <aegavidia@gmail.com>
2018-02-05 20:24:23 +0000

    pihole.sh

---
+++ pihole.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+IP_LOOKUP="$(ip route get 8.8.8.8 | awk '{ print $NF; exit }')"  # May not work for VPN / tun0
+IPv6_LOOKUP="$(ip -6 route get 2001:4860:4860::8888 | awk '{ print $10; exit }')"  # May not work for VPN / tun0
+IP="${IP:-$IP_LOOKUP}"  # use $IP, if set, otherwise IP_LOOKUP
+IPv6="${IPv6:-$IPv6_LOOKUP}"  # use $IPv6, if set, otherwise IP_LOOKUP
+DOCKER_CONFIGS="$(pwd)"  # Default of directory you run this from, update to where ever.
+
+echo "IP: ${IP} - IPv6: ${IPv6}"
+
+# Default ports + daemonized docker container
+docker run -d \
+    --name pihole01 \
+    -p 53:53/tcp -p 53:53/udp -p 80:80 \
+    --network devnet \
+    -v "${DOCKER_CONFIGS}/pihole/pihole/:/etc/pihole/" \
+    -v "${DOCKER_CONFIGS}/pihole/dnsmasq.d/:/etc/dnsmasq.d/" \
+    -e ServerIP="${IP:-$(ip route get 8.8.8.8 | awk '{ print $NF; exit }')}" \
+    -e ServerIPv6="${IPv6:-$(ip -6 route get 2001:4860:4860::8888 | awk '{ print $10; exit }')}" \
+    -e TZ="america/new_york" \
+    --restart=always \
+    diginc/pi-hole-multiarch:debian_armhf
+
